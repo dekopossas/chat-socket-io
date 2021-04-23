5451a7ad51a9c92d4245ae1c2f3c60f37b09c0fb
deko possas <andrepossaster@gmail.com>
2021-04-23 20:38:19 -0300

    componente de mensagem

---
+++ chat-socket-io/src/components/MessageBox/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MenssageBox() {
+  return (
+    <div class="msg right-msg">
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">Sajad</div>
+          <div class="msg-info-time">12:46</div>
+        </div>
+
+        <div class="msg-text">You can change your name in JS section!</div>
+      </div>
+    </div>
+  );
+}
+
+export default MenssageBox;
