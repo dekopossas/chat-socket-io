d111b1dfff816dd434184d5694f948c3a6cc1cba
deko possas <andrepossaster@gmail.com>
2021-04-23 20:52:50 -0300

    manipulando variaveis

--- chat-socket-io/src/components/MessageBox/index.js
+++ chat-socket-io/src/components/MessageBox/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-function MenssageBox({ isMine }) {
+function MenssageBox({ isMine, user, sendAt, message }) {
   const positionClass = (isMine) ? 'right-msg' : 'left-msg';
   return (
     <div className={`msg ${positionClass}`}>
       <div className="msg-bubble">
         <div className="msg-info">
-          <div className="msg-info-name">Sajad</div>
-          <div className="msg-info-time">12:46</div>
+          <div className="msg-info-name">{user}</div>
+          <div className="msg-info-time">{sendAt}</div>
         </div>
-
-        <div className="msg-text">You can change your name in JS section!</div>
+          {message}
+        <div className="msg-text">
+          </div>
       </div>
     </div>
   );
