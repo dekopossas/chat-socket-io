3db3ab36f854f2eb64fc61cd5700198b59e7f1ec
deko possas <andrepossaster@gmail.com>
2021-04-23 20:47:45 -0300

    trabalhando as classes do componente

--- chat-socket-io/src/components/MessageBox/index.js
+++ chat-socket-io/src/components/MessageBox/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 function MenssageBox({ isMine }) {
   const positionClass = (isMine) ? 'right-msg' : 'left-msg';
   return (
-    <div class="msg right-msg">
-      <div class="msg-bubble">
-        <div class="msg-info">
-          <div class="msg-info-name">Sajad</div>
-          <div class="msg-info-time">12:46</div>
+    <div className={`msg ${positionClass}`}>
+      <div className="msg-bubble">
+        <div className="msg-info">
+          <div className="msg-info-name">Sajad</div>
+          <div className="msg-info-time">12:46</div>
         </div>
 
-        <div class="msg-text">You can change your name in JS section!</div>
+        <div className="msg-text">You can change your name in JS section!</div>
       </div>
     </div>
   );
