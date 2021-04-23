62e82df186914e0b423e0b5077422be97f1e7a9e
deko possas <andrepossaster@gmail.com>
2021-04-23 20:42:33 -0300

    transformei a msg em um componente

--- chat-socket-io/src/pages/Chat/index.js
+++ chat-socket-io/src/pages/Chat/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import MessageBox from '../../components/MessageBox';
+
 import './styles.css'
 
 function Chat() {
@@ -17,39 +19,7 @@ function Chat() {
       </header>
 
       <main class="msger-chat">
-        <div class="msg left-msg">
-          <div
-            class="msg-img"
-            style="background-image: url(https://image.flaticon.com/icons/svg/327/327779.svg)"
-          ></div>
-
-          <div class="msg-bubble">
-            <div class="msg-info">
-              <div class="msg-info-name">BOT</div>
-              <div class="msg-info-time">12:45</div>
-            </div>
-
-            <div class="msg-text">
-              Hi, welcome to SimpleChat! Go ahead and send me a message. 😄
-            </div>
-          </div>
-        </div>
-
-        <div class="msg right-msg">
-          <div
-            class="msg-img"
-            style="background-image: url(https://image.flaticon.com/icons/svg/145/145867.svg)"
-          ></div>
-
-          <div class="msg-bubble">
-            <div class="msg-info">
-              <div class="msg-info-name">Sajad</div>
-              <div class="msg-info-time">12:46</div>
-            </div>
-
-            <div class="msg-text">You can change your name in JS section!</div>
-          </div>
-        </div>
+        <MessageBox />
       </main>
 
       <form class="msger-inputarea">
