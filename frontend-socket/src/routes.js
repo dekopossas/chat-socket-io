d3bac31e82ec10030c3e7a58fb6a9e11cf73f0d6
deko possas <andrepossaster@gmail.com>
2021-04-24 19:11:48 -0300

    rotas correcoes

--- frontend-socket/src/routes.js
+++ frontend-socket/src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Login from 'react-router-dom';
+import Login from './pages/Login';
 import Chat from './pages/Chat';
 
 function Routes() {
   return (
     <Switch>
+      <Route path="/" exact component={Login}/>
       <Route path="/login" exact component={Login}/>
       <Route path="/chat" exact component={Chat}/>
     </Switch>
