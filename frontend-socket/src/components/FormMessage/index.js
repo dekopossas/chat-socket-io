18eebd99ad17f9cdf0d21ede711354f588b25df6
deko possas <andrepossaster@gmail.com>
2021-04-23 22:14:36 -0300

    manupulando componente para enviar a msg

--- frontend-socket/src/components/FormMessage/index.js
+++ frontend-socket/src/components/FormMessage/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FormMessage() {
+  const [mensage, setMessage] = useState('');
+
+  const handleSend = (event) => {
+    console.log(message);
+  }
+
   return (
     <>
       <form class="msger-inputarea">
-        <input type="text" class="msger-input" placeholder="Digite uma mensagem..." />
-        <button type="submit" class="msger-send-btn">
+        <input
+          type="text"
+          class="msger-input"
+          placeholder="Digite uma mensagem..."
+          onChange={(event) => handleSend(event.target.value)}
+        />
+        <button
+          type="submit"
+          class="msger-send-btn"
+          onClick={handleSend}
+        >
           Enviar
         </button>
       </form>
