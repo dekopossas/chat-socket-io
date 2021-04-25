2b0cd8aae6e82a38038355121b00e3e78d9f4cba
deko possas <andrepossaster@gmail.com>
2021-04-24 21:59:57 -0300

    criado a requisicao

--- frontend-socket/src/pages/ChatList/index.js
+++ frontend-socket/src/pages/ChatList/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function ChatList() {
-  return (
+  const [ users, setUsers ] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/users').then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+    
+    return (
     <div>
-      
+      <h1>Lista de Usuarios</h1>
+
+      <ul>
+        {users.map(({ username }) => (
+          <li>{username}</li>
+          ))}
+      </ul>
     </div>
   )
 }
-
+  
 export default ChatList;
